83121afc5384ef22483e0309d9d415378ab3261f
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-14 15:55:09 +0500

    add: default

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -6,6 +6,6 @@ switch (rule) {
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break;
-    default:
+    case rule.length < 25:
         console.log("Все таки нет правил без исключения");
 }
