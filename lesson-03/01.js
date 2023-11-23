baec0324d196e4dc32afedf3379b27c4ac8fca77
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-24 01:11:31 +0500

    add: modified function

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,6 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 function createColorString() {
-    return colors.filter(el => el === "черный" || el === "красный" || el === "желтый");
+    return colors.filter(el => el === "черный" || el === "красный" || el === "желтый").join('-');
 }
-console.log(createColorString().join('-'))
+console.log(createColorString());
 
