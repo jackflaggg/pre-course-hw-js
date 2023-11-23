5efa4b842643660aa6209df28d00804749918ea0
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-24 01:33:16 +0500

    add: modified function

--- lesson-02/08.js
+++
@@ -1,7 +0,0 @@
-const currentValue = null;
-if (typeof currentValue === "number") {
-    const result = currentValue  % 2 === 0 ? "Четное число" : "Нечетное число";
-    console.log(result);
-} else {
-    console.log("Это вообще не число");
-}
